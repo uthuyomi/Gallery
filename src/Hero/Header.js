42d1ea4313668566dc59259a3b55172dc1681c9d
yassann20 <kaiseif4e@gmail.com>
2025-06-03 08:09:04 +0900

    auto-commit

--- src/Hero/Header.js
+++ src/Hero/Header.js
@@ -3,18 +3,32 @@ import style from "../scss/hero/Hero.module.scss";
 import Nav from "./Nav.js";
 import { useTheme } from "../ThemeContext"; // ← 必要なら相対パス調整
 
+import lightIcon from "../assets/button/button-light.png";
+import darkIcon from "../assets/button/button-dark.png";
+
 function Header() {
   const [open, setOpen] = useState(false);
   const { mode, toggleMode } = useTheme(); 
+
+  const icon = mode === "light" ? lightIcon : darkIcon;
 
   return (
     <header>
       <div className={style.logo}>
         <img src="" />
       </div>
-      <button onClick={toggleMode} className={style.themeButton}>
-        {mode}
-      </button>
+      <button
+        onClick={toggleMode}
+        className={style.themeButton}
+        style={{
+          background: `url(${icon}) no-repeat center/contain`,
+          width: "120px",
+          height: "48px",
+          border: "none",
+          cursor: "pointer",
+          backgroundColor: "transparent",
+        }}
+      />
       <div
         className={`${style.hum} ${open ? style.open : ""}`}
         onClick={() => setOpen(!open)}
