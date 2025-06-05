321f4367bbb3d3c79843fc113f518b8d8efaf3a2
yassann20 <kaiseif4e@gmail.com>
2025-06-06 00:16:04 +0900

    auto-commit

--- src/Gallery/GalleryListText.js
+++ src/Gallery/GalleryListText.js
@@ -1,11 +1,14 @@
-function GalleryListText({ item }) { 
-    return (
+function GalleryListText({ item, link }) { 
+  return (
+    <>
       <ul>
         {item.map((skill) => (
           <li>{skill}</li>
         ))}
       </ul>
-    );
+      <a href={link}>MORE</a>
+    </>
+  );
 }
 
 export default GalleryListText;
